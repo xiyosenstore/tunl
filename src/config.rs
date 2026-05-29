b8a4af5a2b523bddbf8bc84ba217471a2c086956
ziyosen <ziyosenstore@gmail.com>
2026-05-30 04:21:20 +0700

    Reorder fields in Config struct

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,9 @@
-// src/config.rs
 use uuid::Uuid;
 
 pub struct Config {
     pub uuid: Uuid,
-    pub host: String,               
-    pub proxy_list_url: String,     
-    pub proxy_addr: String,         
-    pub proxy_port: u16,            
+    pub host: String,
+    pub proxy_list_url: String,
+    pub proxy_addr: String,
+    pub proxy_port: u16,
 }
