4e1b874ef517c4369cc8fa31c0f7db2830b14e1f
ziyosen <ziyosenstore@gmail.com>
2026-05-30 04:04:22 +0700

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,10 @@
+// src/config.rs
 use uuid::Uuid;
 
 pub struct Config {
     pub uuid: Uuid,
-    pub host: String,
+    pub host: String,               
+    pub proxy_list_url: String,     
+    pub proxy_addr: String,         
+    pub proxy_port: u16,            
 }
